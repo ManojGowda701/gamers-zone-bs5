9070423d8f0560d494665838f52fecb00241baf6
ManojGowda701 <manojgowda701@gmail.com>
2023-06-29 17:09:36 +0530

    operators

---
+++ Manoj/JS/operator.js
@@ -0,0 +1,45 @@
+let num1 = "manoj";
+let num2 = "sumanth";
+let ans = num1 !== num2;
+console.log(ans);
+
+let num1 = "2";
+let num2 = 2;
+let ans = num1 !== num2;
+console.log(ans);
+
+let num1 = "2";
+let num2 = 2;
+let ans = num1 != num2;
+console.log(ans);
+
+let a = 10 ;
+let b = '10';
+let c = a==b;
+console.log(c);
+
+let x = 10 ;
+let y = '10';
+let z = x===y;
+console.log(z);
+
+let a = 20 ;
+let b = 25;
+let z = a > b;
+console.log(z);
+
+let a = 20 ;
+let b = 25;
+let z = a >= b;
+console.log(z);
+
+let a = 20 ;
+let b = 25;
+let z = a < b;
+console.log(z);
+
+let a = 20 ;
+let b = 25;
+let z = a <= b;
+console.log(z);
+ 
