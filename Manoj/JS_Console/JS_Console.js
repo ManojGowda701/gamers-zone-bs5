13139a4e4695b0ffef6a4d9adbf83c3f70f27b24
ManojGowda701 <manojgowda701@gmail.com>
2023-08-26 07:10:08 +0530

    Javascript Console

---
+++ Manoj/JS_Console/JS_Console.js
@@ -0,0 +1,16 @@
+let title = "Welcome to E-Learning!";
+const name = "Manoj"; 
+console.log(name + " " + title);            //Manoj Welcome to E-Learning!
+
+
+//-------------------------------------------
+
+let title = "Welcome to E-Learning!";
+const name = "Manoj"; 
+console.log(name + title);          //ManojWelcome to E-Learning!
+
+//-------------------------------------------
+
+let title = "Welcome to E-Learning!";
+const name = "Manoj"; 
+console.log(name , title);          //Manoj Welcome to E-Learning!
